ca0ed303608b9f47abf491c964f6f8e1ce110fc5
Katsu <comeaga@mail.ru>
2025-11-21 20:21:00 +0300

    вернул ассерты

--- tests/test_constructor_sections.py
+++ tests/test_constructor_sections.py
@@ -12,6 +12,8 @@ class TestConstructorTabs:
 
         WebDriverWait(driver, 5).until(EC.text_to_be_present_in_element((ACTIVE_TAB), 'Начинки'))
 
+        assert driver.find_element(*ACTIVE_TAB).text == 'Начинки'
+
     def test_move_to_buns_section_button(self, driver):
         driver.get("https://stellarburgers.education-services.ru/")
         driver.find_element(*FILLINGS_BUTTON).click()
@@ -19,8 +21,12 @@ class TestConstructorTabs:
 
         WebDriverWait(driver, 5).until(EC.text_to_be_present_in_element((ACTIVE_TAB), 'Булки'))
 
+        assert driver.find_element(*ACTIVE_TAB).text == 'Булки'
+
     def test_move_to_sauces_section_button(self, driver):
         driver.get("https://stellarburgers.education-services.ru/")
         driver.find_element(*SAUCES_BUTTON).click()
 
         WebDriverWait(driver, 5).until(EC.text_to_be_present_in_element((ACTIVE_TAB), 'Соусы'))
+
+        assert driver.find_element(*ACTIVE_TAB).text == 'Соусы'
